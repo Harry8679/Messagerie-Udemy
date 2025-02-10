6d63d83a01891a9e69b5369eeca8403558037038
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-10 14:35:58 +0100

    Library installed and Login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,23 @@
-import { auth, googleProvider } from "../firebaseConfig";
-import { signInWithPopup, signOut } from "firebase/auth";
+import React from "react";
+import { signInWithGoogle, logOut, auth } from "../firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Login() {
-  const signIn = async () => {
-    await signInWithPopup(auth, googleProvider);
-  };
-
-  const logout = async () => {
-    await signOut(auth);
-  };
+  const [user] = useAuthState(auth);
 
   return (
     <div className="p-4">
-      {auth.currentUser ? (
-        <div>
-          <p>Bienvenue, {auth.currentUser.displayName}</p>
-          <button onClick={logout} className="bg-red-500 p-2 text-white">Déconnexion</button>
-        </div>
+      {user ? (
+        <>
+          <p>Bienvenue, {user.displayName}</p>
+          <button onClick={logOut} className="bg-red-500 p-2 text-white">
+            Déconnexion
+          </button>
+        </>
       ) : (
-        <button onClick={signIn} className="bg-blue-500 p-2 text-white">Se connecter avec Google</button>
+        <button onClick={signInWithGoogle} className="bg-blue-500 p-2 text-white">
+          Se connecter avec Google
+        </button>
       )}
     </div>
   );
