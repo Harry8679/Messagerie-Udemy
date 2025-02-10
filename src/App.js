01cb03ca7c8a0f1e2629b45f37d3bf81756b52b4
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-10 14:27:44 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import ChatRoom from "./components/ChatRoom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<ChatRoom />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
